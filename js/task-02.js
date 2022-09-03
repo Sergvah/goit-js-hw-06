7e76220f9df0c4570a047538715adf334ce262a4
Sergvah <104735864+Sergvah@users.noreply.github.com>
2022-09-03 16:37:36 +0300

    task - 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
+"use strict";
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+let ulList = document.getElementById("ingredients");
+
+// console.log(ulList);
+
+const product = ingredients.forEach(function (item) {
+  let itemsLi = document.createElement("li");
+  itemsLi.classList.add("item");
+  itemsLi.innerHTML = item;
+  ulList.append(itemsLi);
+  // console.log(itemsLi);
+});
+
+console.log(product);
