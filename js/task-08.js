a2891a5c6232c4b3c29e284b1548ac7eb03c5bd8
Sergvah <104735864+Sergvah@users.noreply.github.com>
2022-09-04 17:05:03 +0300

    task - 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+"use strict";
 
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", onSubmit);
+const data = {};
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Все поля должны быть заполнены");
+  }
+
+  const el = event.currentTarget.elements;
+  data[el.email.name] = el.email.value;
+  data[el.password.name] = el.password.value;
+  console.log(data);
+  event.currentTarget.reset();
+}
