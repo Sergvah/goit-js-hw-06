ec61e1e9e6171de251eac0cdb87f58d897f0f65e
Sergvah <104735864+Sergvah@users.noreply.github.com>
2022-09-04 19:59:47 +0300

    task - 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+"use strict";
+
+const widg = document.querySelector(".widget");
+const btnChangeColor = document.querySelector(".change-color");
+const currentColor = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", bodyBgcr);
+function bodyBgcr() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  currentColor.textContent = getRandomHexColor();
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
